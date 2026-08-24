f262b9dd291462df9c627d5e96d4e2202f62030e
Karina <karina@karter.dev>
2026-08-24 05:41:34 +0400

    feat(core): add positioned and range lookup request contracts

--- Core/Sources/TsubameCore/Model/LookupResult.swift
+++ Core/Sources/TsubameCore/Model/LookupResult.swift
@@ -5,3 +5,12 @@
 //  Created by k on 21.08.2026.
 //
 
+public struct LookupResult: Sendable, Equatable {
+    public let sourceRange: UTF8TextRange
+    public let entries: [DictionaryEntry]
+
+    public init(sourceRange: UTF8TextRange, entries: [DictionaryEntry]) {
+        self.sourceRange = sourceRange
+        self.entries = entries
+    }
+}
